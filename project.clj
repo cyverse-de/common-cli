41603cca0cc52a11158603097029a815f5d9057a
Ian McEwen <mian@cyverse.org>
2016-08-30 14:20:27 -0700

    Version 2.8.0 (+switch to org.cyverse)

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject org.iplantc/common-cli "5.2.8.0"
+(defproject org.cyverse/common-cli "2.8.0"
   :description "Common CLI functions for the DE backend services and tools"
-  :url "http://github.com/iPlantCollaborativeOpenSource/DiscoveryEnvironmentBackend/"
+  :url "http://github.com/cyverse-de/common-cli"
   :license {:name "BSD"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.2"]
                  [trptcolin/versioneer "0.1.0"]])
