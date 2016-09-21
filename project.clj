ac2b5313240b278e973d089a8dd8470f84bbc495
Ian McEwen <mian@cyverse.org>
2016-09-21 11:46:45 -0700

    Add test2junit, publish test results, and remove AbortException case in Jenkinsfile

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "BSD"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
+  :plugins [[test2junit "1.2.2"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.2"]
                  [trptcolin/versioneer "0.1.0"]])
