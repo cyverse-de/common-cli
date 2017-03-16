832e5050a404fd52551ad69e2731ae4c54a88221
Ian McEwen <mian@cyverse.org>
2017-03-16 12:53:15 -0700

    Version 2.8.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/common-cli "2.8.1"
+(defproject org.cyverse/common-cli "2.8.2-SNAPSHOT"
   :description "Common CLI functions for the DE backend services and tools"
   :url "http://github.com/cyverse-de/common-cli"
   :license {:name "BSD"}
